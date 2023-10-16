8dee6cc38a250c1e6fa2c78772233c714fd300e1
Azib Farooq <64292764+itsazibfarooq@users.noreply.github.com>
2023-10-16 11:39:35 -0400

    fixing bugs in about

--- components/about.tsx
+++ components/about.tsx
@@ -19,7 +19,7 @@ const About = () => {
           <p className="uppercase text-xl tracking-widest text-[#5651e5]">
             About
           </p>
-          <h2 className="py-4">Hi, I'm Azib 🤝</h2>
+          <h2 className="py-4">Hi, I&apos;m Azib 🤝</h2>
           <p className="py-2 text-[#808080] font-mono">\\ Hello world 👋</p>
         <p className="py-2 text-gray-600">
         If I have to describe myself in a single word, it would be &quot;self-starter🏃&quot;.
