4aa7b9d9f868b3ff18a406dfb07eb514e333305a
azib <azibfarooq10@gmail.com>
2023-12-04 19:18:38 -0500

    udpated experience

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -8,7 +8,7 @@ function Experience() {
     <div id="projects" className="w-full p-2 pt-12">
       <div className="max-w-[80%] mx-auto px-2 py-16">
         <p className="text-xl tracking-widest uppercase text-[#5651e5]">
-          Projects
+          Experience
         </p>
         <h2 className="py-4">Past Workplaces</h2>
         <div className="grid md:grid-cols-2 gap-8">
